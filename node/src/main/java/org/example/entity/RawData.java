a2197ab1dc79b51da81c6c0636a1def1459361c7
hreenir <hreenir12@gmailc.com>
2023-07-06 15:51:49 +0300

    add postgresql

--- node/src/main/java/org/example/entity/RawData.java
+++ node/src/main/java/org/example/entity/RawData.java
@@ -1,13 +1,10 @@
 package org.example.entity;
 
-import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.Type;
-import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 import org.telegram.telegrambots.meta.api.objects.Update;
-
-import javax.persistence.*;
 
 @Getter
 @Setter
@@ -17,12 +14,11 @@ import javax.persistence.*;
 @AllArgsConstructor
 @Entity
 @Table(name = "raw_data")
-@TypeDef(name = "jsonb", typeClass = JsonBinaryType.class)
 public class RawData {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Type(type = "jsonb")
+    @JdbcTypeCode(SqlTypes.JSON)
     @Column(columnDefinition = "jsonb")
     private Update event;
 }
